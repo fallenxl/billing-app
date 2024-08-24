014fcb02d182d1f83ad38c2e87e84ed4b2cfb855
FallEn <axlsntz.dev@gmail.com>
2024-08-23 23:14:48 -0600

    added changes

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,3 +1,3 @@
 export default  {
-    API: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/billing/v1' : 'https://api.example.com',
+    API: process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api/billing/v1' : 'https://api.lumenenergysolutions.com/api/billing/v1',
 }
